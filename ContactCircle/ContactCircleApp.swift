173cbbbc65f5c9907af79ea8923aa1715b371044
Maria Sandu <Maria.Sandu@emids.com>
2023-09-22 22:22:51 +0300

    Initial Commit

---
+++ ContactCircle/ContactCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactCircleApp.swift
+//  ContactCircle
+//
+//  Created by Maria Sandu on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
